42844d12a0f1881b13bb77f766d1a66471d3c728
fatdoge <704274491@qq.com>
2018-06-13 23:50:54 +0800

    init

---
+++ db/dbConfig.js
@@ -0,0 +1,10 @@
+module.exports =
+    {
+        mysql: {
+            host: 'fatdoge.cn',
+            user: 'yzedu',
+            password: 'xuke19971217',
+            database: 'yzedu', // 前面建的user表位于这个数据库中 
+            port: 3306
+        }
+    };
---
+++ db/userSql.js
@@ -0,0 +1,9 @@
+var UserSQL = {
+    insert: 'INSERT INTO userAccounts(username,password) VALUES(?,?)',
+    queryAll: 'SELECT * FROM userAccounts',
+    getUserById: 'SELECT * FROM userAccounts WHERE id = ? ',
+    getUserByUsername: 'SELECT COUNT(*) FROM userAccounts WHERE username = ? ',
+    isUser: 'SELECT COUNT(*) from userAccounts WHERE username = ? AND password = ?',
+    getUserInfo: 'SELECT * FROM userInfo WHERE username = ?'
+};
+module.exports = UserSQL;
